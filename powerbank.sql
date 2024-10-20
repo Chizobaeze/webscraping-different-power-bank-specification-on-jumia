2e5c3ce87512abc8867ebe353c8f53339688e83a
Eze Chizoba Cynthia <chizobaeze15@gmail.com>
2024-10-20 20:35:40 +0100

    Add files via upload

---
+++ powerbank.sql
@@ -0,0 +1,44 @@
+/*
+-- Query: SELECT * FROM powerbank.powerbank
+LIMIT 0, 1000
+
+-- Date: 2024-10-20 20:31
+*/
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (1,'Pining Power Bank 20000Mah 20000MAh Intelligent Original  TYPE C Fast Charge Power Bank With Powerbank Display For Cheap (Finnary)','â‚¦ 15,965','â‚¦ 18,000','11%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (2,'Recresi 20000mah NEWAGE PD Powerbank Original 20000Mah AI Strong Power Bank With Fast Charge 22\'500mah  TYPE C INPUT OUTPUT (y107)','â‚¦ 21,990','â‚¦ 42,900','49%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (3,'Pining Power Bank 20000Mah NEWAGE COMPLETE Powerbank Original 22500MÃ¡h AI Strong PD Power Bank With Fast Charge TYPE C INPUT And OUTPUT 22500 Capacity (J153)','â‚¦ 28,600','â‚¦ 58,960','51%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (4,'Recresi 20000mah 20000MAh Original Powerbank Fast Charging 22.5W Intelligent Power Bank-Ora Display At Cheap Price','â‚¦ 23,990','â‚¦ 32,500','26%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (5,'Pining Power Bank 20000Mah WIRELESS Power Bank For IPhone & Samsung Type C USB','â‚¦ 32,000','â‚¦ 55,000','42%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (6,'Recresi 20000mah 20000 MAh Power Bank Fast Charging PowerBank','â‚¦ 36,000','â‚¦ 55,900','36%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (7,'Pining Power Bank 20000Mah WIRELESS For Iphone & Samsung VEGER VP-1027W âœ…âœ…','â‚¦ 34,000','â‚¦ 54,000','37%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (8,'Pining Power Bank 20000Mah WIRELESS SUPER CHARGE For IPhone VEGER VP1027w','â‚¦ 34,000','â‚¦ 52,000','35%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (9,'Recresi 20000mah 20000MAh Intelligent Original  Power Bank Ora With Powerbank Display For Cheap','â‚¦ 24,350','â‚¦ 48,600','50%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (10,'Recresi 20000mah WIRELESS Power Bank For IPhone And Samsung VEGER VP1027w','â‚¦ 34,000','â‚¦ 58,000','41%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (11,'Recresi 20000mah ITEL Original Powerbank Fast Charging 22.5W Intelligent Power Bank-Ora Display At Cheap','â‚¦ 24,990','â‚¦ 55,500','55%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (12,'Recresi 20000mah Power Bank 20000mAh PowerBank  Type C Input Output Digital Display','â‚¦ 29,950','â‚¦ 50,000','40%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (13,'Recresi 20000mah WIRELESS Veger Power Bank For Samsung VP-1027W Black Color','â‚¦ 36,500','â‚¦ 50,000','27%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (14,'Recresi 20000mah WIRELESS Power Bank For IPhone & Samsung Galaxy - Quality Super Fast Charging WithType C Port','â‚¦ 32,500','â‚¦ 54,000','40%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (15,'Pining Power Bank 20000Mah WIRELESS For Iphone & Samsung VEGER VP-1027W âœ…âœ…','â‚¦ 35,000','â‚¦ 51,000','31%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (16,'Pining Power Bank 20000Mah WIRELESS For IPhone And Samsung VEGER VP1027w','â‚¦ 36,000','â‚¦ 42,000','14%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (17,'Recresi 20000mah WIRELESS SUPER POWER BANK For IPhone & Samsung VEGER VP1027w','â‚¦ 35,000','â‚¦ 48,000','27%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (18,'itel 20000mAh Dual Output Fast Charging Power Bank','â‚¦ 15,990','â‚¦ 20,000','20%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (19,'itel PowerLite 20000mAh 15W Fast Charger Type C Mobile Power','â‚¦ 17,000','â‚¦ 28,000','39%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (20,'Firman P20H 20000mAh (SUMEC) Power Bank','â‚¦ 27,000','â‚¦ 25,000','25%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (21,'itel 20000mAh Fast Charging, Bright Torch Power Bank + Free Fast Charger','â‚¦ 18,850','â‚¦ 18,500','10%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (22,'itel 20000mAh  Mobile Power Charge Bank','â‚¦ 16,700','â‚¦ 45,000','18%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (23,'Pining Power Bank 20000Mah WIRELESS For Samsung Galaxy- Quality Super Fast Charging WithType C Port','â‚¦ 37,000','â‚¦ 48,843','43%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (24,'Oraimo Type-C And Micro-USB Port Power-Bank 20000mAh Fast Charging','â‚¦ 27,760','â‚¦ 24,000','22%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (25,'itel 20000mAh  Mobile Power Charge Bank + ICW-051U 5W Charger','â‚¦ 18,700','â‚¦ 32,500','23%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (26,'Recresi 20000mah 20000MAh 22.5W Itel Typec Output Power Bank Display Cheap Powerbank','â‚¦ 24,999','â‚¦ 45,000','56%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (27,'Oraimo 20000mAh Powerbank Type-C & Micro-USB Input Fast Charging','â‚¦ 19,999','â‚¦ 38,000','47%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (28,'Oraimo 20000mAh Powerbank Type-C & Micro-USB Input Fast Charging','â‚¦ 19,999','â‚¦ 40,000','48%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (29,'Oraimo 20000mAh Powerbank Type-C & Micro-USB Input Fast Charging','â‚¦ 20,999','â‚¦ 19,990','15%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (30,'XIAOMI Mi 20000mAh Quick Charge Power Bank','â‚¦ 37,000','â‚¦ 28,800','53%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (31,'itel 20000mAh Fast Charging, Bright Torch STAR 200Fast Power Bank','â‚¦ 16,990','â‚¦ 21,510','60%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (32,'Oraimo Traveler 4 20000mAh 10.5W Power Banks','â‚¦ 13,500','â‚¦ 48,000','67%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (33,'Philly 20000mah  New Power Banks Type-C  Input','â‚¦ 8,500','â‚¦ 20,500','3%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (34,'itel PowerPulse A1160 Power-Bank 20000mAh 12W Fast Charging Type-C','â‚¦ 15,918','â‚¦ 20,000','15%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (35,'itel PowerPulse A1460 Power-Bank 20000mAh 20W Fast Charging Type-C','â‚¦ 19,800','â‚¦ 45,650','39%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (36,'itel PowerPulse 20000mAh 12W Fast Charging, Torch Power Bank','â‚¦ 16,990','â‚¦ 23,100','66%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (37,'itel Powerful 20000MAh Intelligent Original Power Bank','â‚¦ 27,700','â‚¦ 15,000','43%');
+INSERT INTO `` (`No`,`productname`,`newprice`,`oldprice`,`discounts`) VALUES (38,'VIC â™›20000mAh Power Banks, Portable Charger High-Speed Charging Battery Pack','â‚¦ 7,950','â‚¦ 29,110','42%');
